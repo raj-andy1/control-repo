37e020fd53b4fa774f006f6b155eb34e9fe1a913
Andy <andyr@puppet.com>
2016-09-19 16:04:06 -0700

    new file:   site/apache/files/info.php modified:   site/apache/manifests/init.pp

--- site/apache/manifests/init.pp
+++ site/apache/manifests/init.pp
@@ -1,12 +1,25 @@
 # /etc/puppetlabs/code/environments/production/site/apache/init.pp
 
 class apache {
+
+  $conf_dir = '/var/www/html'
+
   package { "httpd":
   ensure  =>  present,
   }
+
   service {"httpd":
   ensure  =>  running,
   enable  =>  true,
   require  =>  Package['httpd'],
+  #subscribe =>  File["${conf_dir}/info.php"],
+  }
+
+  file { "${conf_dir}/info.php":
+    ensure  =>  file,
+    mode  =>  '0755',
+    source  =>  'puppet:///apache/info.php',
+    require =>  Package['httpd'],
+    notify  =>  Service['httpd'],
   }
 }

0d86f2400894a22020ccefe97d59ba452015c188
Andy <andyr@puppet.com>
2016-09-19 13:56:12 -0700

    site/mariadb/manifests/init.pp

--- site/mariadb/manifests/init.pp
+++ site/mariadb/manifests/init.pp
@@ -1,8 +1,8 @@
 # /etc/puppetlabs/code/environments/production/site/mariadb/manifests/init.pp
 
 class mariadb {
-  package {'mariadb-server':
-  name  =>  ["mariadb-server","mariadb"],
+  $package_name = ['mariadb-server','mariadb']
+  package { $package_name:
   ensure => present,
   }
   service {"mariadb":

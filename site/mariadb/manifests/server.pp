2f995411b0a7306a45076f0164942341733e1e09
Andy <andyr@puppet.com>
2016-09-21 11:37:51 -0700

    new file:   site/mariadb/manifests/server.pp site/mariadb/.DS_Store site/mariadb/files/

---
+++ site/mariadb/manifests/server.pp
@@ -0,0 +1,25 @@
+# /etc/puppetlabs/code/environments/production/site/mariadb/manifests/server.pp
+
+class mariadb::server{
+
+  $mysql_password = '$1$4laP3JKW$mfhNrVY9/WXeJEDm/Ue.N0'
+
+  file { "/var/lib/mysql/my.cnf":
+    owner => "mysql", group => "mysql",
+    source => "puppet:///mysql/my.cnf",
+    notify => Service["mariadb"],
+    require => Package["mariadb"],
+  }
+
+  file { "/etc/my.cnf":
+    require => File["/var/lib/mysql/my.cnf"],
+    ensure => "/var/lib/mysql/my.cnf",
+  }
+
+  exec { "set-mysql-password":
+    unless => "mysqladmin -uroot -p\"${mysql_password}\" status",
+    path => ["/bin", "/usr/bin"],
+    command => "mysqladmin -uroot password \"${mysql_password}"",
+    require => Service["mariadb"],
+  }
+}

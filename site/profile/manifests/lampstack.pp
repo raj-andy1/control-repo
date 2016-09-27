90a6b011b9cd3ef52385855d8faa1cf1870ae7e4
Andy <andyr@puppet.com>
2016-09-27 14:52:45 -0700

    modified:   site/profile/manifests/lampstack.pp

--- site/profile/manifests/lampstack.pp
+++ site/profile/manifests/lampstack.pp
@@ -12,7 +12,7 @@ class profile::lampstack {
   wp_group  => 'wordpress',
   db_user  => 'wordpress',
   db_password  => '$1$tPyS005D$BO2bEaAK/ufrBYJqcuzel0',
-  install_dir =>  '/var/www/wordpress'
+  install_dir =>  '/var/www/wordpress',
   create_db  => true,
   create_db_user  => true,
   }

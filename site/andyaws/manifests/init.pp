b70d049d20b4a6b0c0b9fcc5da55fc29d03dc79e
Andy <andyr@puppet.com>
2016-10-06 11:16:26 -0700

    deleted:    site/andy-aws/manifests/init.pp modified:   site/andyaws/manifests/init.pp

--- site/andyaws/manifests/init.pp
+++ site/andyaws/manifests/init.pp
@@ -1,10 +1,12 @@
 #change directory
 class andyaws {
-  ec2_instance { 'test-andyr':
+  ec2_instance { 'andyr-puppetmaster':
   ensure  =>  present,
   image_id  =>  'ami-775e4f16',
   instance_type =>  'm3.medium',
   key_name  =>  'andy.rajagopalan',
+  vpc =>  'vpc-e4e0581',
+  iam_instance_profile_name =>  'puppetlabs_aws_provisioner',
   tags  =>  {
       name  => 'andy.rajagopalan',
       department  => 'tse',

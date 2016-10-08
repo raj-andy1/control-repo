e41cfa989c79886d2fd4e609cb698ad1090407ed
Andy <andyr@puppet.com>
2016-10-07 17:37:40 -0700

    modified:   site/andyaws/manifests/init.pp

--- site/andyaws/manifests/init.pp
+++ site/andyaws/manifests/init.pp
@@ -1,7 +1,7 @@
-#change directory
+# /etc/puppetlabs/code/environments/production/site/andyaws/manifests/init.pp
 class andyaws {
   ec2_instance { 'andyr-puppetmaster':
-  ensure  =>  present,
+  ensure  =>  absent,
   image_id  =>  'ami-775e4f16',
   instance_type =>  'm3.medium',
   key_name  =>  'andy.rajagopalan',
@@ -12,5 +12,16 @@ class andyaws {
       project => 'internal-practice',
       created_by => 'andyr@puppet.com',
     }
+  ec2_instance { 'andyr-puppetmaster01':
+    ensure  =>  present,
+    image_id  =>  'ami-775e4f16',
+    instance_type =>  'm3.large',
+    key_name  =>  'andy.rajagopalan',
+    iam_instance_profile_name =>  'puppetlabs_aws_provisioner',
+    tags  =>  {
+        name  => 'andy.rajagopalan',
+        department  => 'tse',
+        project => 'internal-practice',
+        created_by => 'andyr@puppet.com',
   }
 }

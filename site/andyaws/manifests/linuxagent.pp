1fb4d13aa4c6ef8e1444fde9a843944a838baa22
Andy <andyr@puppet.com>
2016-10-10 10:40:25 -0700

    new file:   site/andyaws/manifests/linuxagent.pp

---
+++ site/andyaws/manifests/linuxagent.pp
@@ -0,0 +1,17 @@
+# /etc/puppetlabs/code/environments/production/site/andyaws/manifests/linuxagent.pp
+
+class linuxagent {
+  $pe_master_hostname   = 'ip-your-ip-here.us-west-2.compute.internal'
+  $pe_version_string    = '2015.2.2'
+  $agent_version_string = '1.2.6'
+
+  ec2_instance { 'puppet-agent':
+    ensure          => present,
+    region          => 'us-west-2',
+    image_id        => 'ami-e08efbd0', # RHEL 6.5
+    instance_type   => 'm3.medium',
+    monitoring      => 'true',
+    security_groups => ['puppet'],
+    user_data       => template('agent-pe-userdata.erb'),
+  }
+}

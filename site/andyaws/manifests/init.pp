a04afcc7b4bcbcfb4ab29969711eab73be7789ba
Andy <andyr@puppet.com>
2016-10-10 09:48:48 -0700

    modified:   site/andyaws/manifests/init.pp

--- site/andyaws/manifests/init.pp
+++ site/andyaws/manifests/init.pp
@@ -3,6 +3,7 @@ class andyaws {
   ec2_instance { 'andyr-puppetmaster':
   ensure  =>  stopped,
   name  =>  'andyr-puppetmaster'
+  image_id  =>  'ami-775e4f16',
     }
   ec2_instance { 'andyr-puppetmaster01':
     ensure  =>  present,

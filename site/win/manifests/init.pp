e81bd818b2ac2b361e540b81d4f1589a42663154
Andy <andyr@puppet.com>
2016-10-11 12:25:57 -0700

    modified:   site/win/manifests/init.pp

--- site/win/manifests/init.pp
+++ site/win/manifests/init.pp
@@ -1,1 +1,7 @@
 # /etc/puppetlabs/code/environments/production/site/win/manifests/init.pp
+class xom {
+  registry_key { 'HKLM\System\CurrentControlSet\Services\Puppet':
+  ensure  =>  present,
+
+  }
+}

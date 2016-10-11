f2b19aea0790748d4ba99c8a6c637d2b43886456
Andy <andyr@puppet.com>
2016-10-11 15:42:52 -0700

    modified:   site/win/manifests/init.pp

--- site/win/manifests/init.pp
+++ site/win/manifests/init.pp
@@ -1,7 +1,11 @@
 # /etc/puppetlabs/code/environments/production/site/win/manifests/init.pp
 class win {
   registry_key { 'HKLM\System\CurrentControlSet\Services\Puppet01':
-  ensure  =>  absent,
-
+  ensure  =>  present,
   }
+
+ registry::value { 'Puppet02':
+  key =>  'HKLM\System\CurrentControlSet\Services\',
+  data  => 'some user_data',
+ }
 }

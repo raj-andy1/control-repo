2317f66d06d463dd50ab4fc9a00ec30442e31a79
Andy <andyr@puppet.com>
2016-10-12 12:00:23 -0700

    deleted:    site/manage_users/manifests/winusers.pp new file:   site/win/manifests/winusers.pp

---
+++ site/win/manifests/winusers.pp
@@ -0,0 +1,13 @@
+# /etc/puppetlabs/code/environments/production/site/win/manifests/winusers.pp
+
+class winusers{
+  group { 'testgroup01':
+    ensure => present,
+  }
+
+  user { ['testuser01','testuser02']:
+    ensure  =>  present,
+    group =>  'testgroup01',
+    password  => 'Puppetlabs01'
+  }
+}

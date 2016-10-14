cbb4874723b037f375cb5ef096f96ad2c6f887ff
Andy <andyr@puppet.com>
2016-10-14 13:54:45 -0700

    modified:   site/win/manifests/wininstall.pp

--- site/win/manifests/wininstall.pp
+++ site/win/manifests/wininstall.pp
@@ -1,5 +1,5 @@
 # /etc/puppetlabs/code/environments/production/site/win/manifests/winusers.pp
-class win::wininstall.pp {
+class win::wininstall {
   package {'notepadplusplus':
   ensure  =>  latest,
   provider  =>  'chocolatey'

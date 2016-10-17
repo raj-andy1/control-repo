1b2b7c63ffa171c390cc69b0b49bcb3ae565f09b
Andy <andyr@puppet.com>
2016-10-17 12:27:51 -0700

    modified:   site/win/manifests/winfeat.pp

--- site/win/manifests/winfeat.pp
+++ site/win/manifests/winfeat.pp
@@ -1,1 +1,8 @@
 # /etc/puppetlabs/code/environments/production/site/win/manifests/winfeat.pp
+class winfeat{
+  windowsfeature {'Web-Server':
+    ensure  =>  present,
+    installmanagementtools  =>  true,
+    installsubfeatures  =>  true, 
+  }
+}

dcaf3dec7076a9e28f2ce38152da053b98017b84
Andy <andyr@puppet.com>
2016-10-17 11:31:13 -0700

    modified:   site/win/manifests/wininstall.pp modified:   Puppetfile

--- site/win/manifests/wininstall.pp
+++ site/win/manifests/wininstall.pp
@@ -1,7 +1,12 @@
 # /etc/puppetlabs/code/environments/production/site/win/manifests/winusers.pp
 class win::wininstall {
   package {'notepadplusplus':
-  ensure  =>  latest,
-  provider  =>  'chocolatey'
+    ensure  =>  latest,
+    provider  =>  'chocolatey',
+  }
+
+  package {'securecrt':
+    ensure => '7.3.7',
+    provider  =>  'chocolatey',
   }
 }

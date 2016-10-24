5446b53f83e135bb67870c667aa955d9db72cc56
Andy <andyr@puppet.com>
2016-10-24 16:41:56 -0700

    modified:   site/arntp/manifests/init.pp

--- site/arntp/manifests/init.pp
+++ site/arntp/manifests/init.pp
@@ -1,1 +1,11 @@
 # /etc/puppetlabs/code/environments/production/site/arntp/manifests/init.pp
+
+class arntp {
+  class  {'ntp':
+  $ntpservernm = $::os["name"]? {
+    'Redhat'  =>  'nist-time-server.eoni.com',
+    'windows' =>  'nist1-sj.witime.net'
+      }
+    servers => $ntpservernm,
+    }
+  }

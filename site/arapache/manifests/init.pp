e72bd74f1b98e708d7436f92610318cc1221e9ef
Andy <andyr@puppet.com>
2016-10-31 13:45:37 -0700

    new file:   site/arapache/files/info.php new file:   site/arapache/manifests/init.pp new file:   site/aropenssh/files/sshd_config new file:   site/aropenssh/manifests/init.pp

---
+++ site/arapache/manifests/init.pp
@@ -0,0 +1,22 @@
+# /etc/puppetlabs/code/environments/production/site/arapache/manifests/init.pp
+
+class arapache {
+
+  package { 'httpd':
+  ensure  =>  present,
+  }
+
+  service { 'httpd':
+  ensure  =>  running,
+  enable  =>  true,
+  require  =>  Package['httpd'],
+  }
+
+  file { '/var/www/html/info.php':
+    ensure  =>  file,
+    mode  =>  '0755',
+    source  =>  'puppet:///modules/apache/info.php',
+    require =>  Package['httpd'],
+    notify  =>  Service['httpd'],
+  }
+}
---
+++ site/aropenssh/manifests/init.pp
@@ -0,0 +1,22 @@
+# /etc/puppetlabs/code/environments/production/site/aropenssh/manifests/init.pp
+
+class aropenssh {
+
+  package { 'openssh-server':
+    ensure  => present,
+  }
+
+  service { 'ssh':
+    subscribe =>  File['sshdconfig'],
+    require =>  Package['openssh-server'],
+  }
+
+  file { 'sshdconfig':
+    name  => '/etc/ssh/sshd_config',
+    owner => root,
+    group =>  root,
+    mode => 644,
+    source  =>  'puppet:///modules/aropenssh/sshd_config',
+    require =>  Package['openssh-server'],
+  }
+}

7c68dc87e497204129cbe17c6d4409c7d0b952f1
Andy <andyr@puppet.com>
2016-11-22 06:16:47 -0800

    modified:   site/arinifile/manifests/init.pp

--- site/arinifile/manifests/init.pp
+++ site/arinifile/manifests/init.pp
@@ -1,7 +1,8 @@
-
+# /etc/puppetlabs/code/environments/production/site/arinifile/manifests/init.pp
+# Sample code to change Puppet agent run interval to 10d from 30m
 
 class arinifile {
-  ini_setting { 'change runinterval to 5d':
+  ini_setting { 'change runinterval to 10d':
     ensure  => present,
     path    => '/etc/puppetlabs/puppet/puppet.conf',
     section => 'agent',

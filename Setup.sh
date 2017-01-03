1cce03c163b85a492e667f9bc1abb7a423319e76
Andy <andyr@puppet.com>
2017-01-03 13:29:47 -0800

    modified:   Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -9,12 +9,4 @@ master_name=andyr-demomaster
 agent_url="https://"$master_name":8140/packages/current/install.bash"
 echo $master_ip $master_name.localdomain  $master_name >> /etc/hosts
 echo $(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)  $host_name  >> /etc/hosts
-curl -k $agent_url | bash
-cat > /etc/puppetlabs/puppet/csr_attributes.yaml  <<  YAML
-extension_requests:
-  pp_role: demo-webserver
-  pp_created_by:  AndyR
-  pp_department: TSE
-  pp_datacenter:  PuppetHQ
-YAML
-puppet agent -t
+curl -k $agent_url | bash -s extension_requests:pp_role=demo-webserver pp_created_by=AndyR pp_department=TSE

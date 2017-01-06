945046bc19040d8c6a3f62f7ae2a8aa3f6938578
Andy <andyr@puppet.com>
2017-01-06 13:02:55 -0800

    modified:   Setup.sh modified:   WinSetup.ps1

--- Setup.sh
+++ Setup.sh
@@ -9,4 +9,4 @@ master_name=andyr-demomaster
 agent_url="https://"$master_name":8140/packages/current/install.bash"
 echo $master_ip $master_name.localdomain  $master_name >> /etc/hosts
 echo $(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)  $host_name  >> /etc/hosts
-curl -k $agent_url | bash -s extension_requests:pp_role=demo-webserver pp_created_by=AndyR pp_department=TSE
+curl -k $agent_url | bash -s extension_requests:pp_role=demo-webserver extension_requests:pp_created_by=AndyR extension_requests:pp_department=TSE
--- WinSetup.ps1
+++ WinSetup.ps1
@@ -1,2 +1,5 @@
 #ps1
 # Setup script for preparing windows virtual machines to work with Puppet master
+
+ac -Encoding UTF8  C:\Windows\system32\drivers\etc\hosts "192.168.0.10	master4.localdomain	master4"
+[Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}; $webClient = New-Object System.Net.WebClient; $webClient.DownloadFile('https://master4:8140/packages/current/install.ps1', 'install.ps1'); .\install.ps1

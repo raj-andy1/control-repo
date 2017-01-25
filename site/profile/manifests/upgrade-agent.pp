c4eee2a827d01e5d7888ea7bc9c193e14e20d5e6
Andy <andyr@puppet.com>
2017-01-24 16:25:53 -0800

    modified:   site/profile/manifests/upgrade-agent.pp

---
+++ site/profile/manifests/upgrade-agent.pp
@@ -0,0 +1,5 @@
+# /etc/puppetlabs/code/environments/production/site/profile/manifests/upgrade-agent.pp
+# Sample code to use the puppet agent module to upgrade older versions of puppet agent
+
+
+include ::puppet_agent

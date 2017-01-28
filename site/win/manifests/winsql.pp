ac5904bd19bff22b3755ab8c53e749e033280ae1
Andy <andyr@puppet.com>
2017-01-28 15:12:01 -0800

    modified:   site/win/manifests/winsql.pp

--- site/win/manifests/winsql.pp
+++ site/win/manifests/winsql.pp
@@ -1,13 +1,14 @@
 # /etc/puppetlabs/code/environments/production/site/win/manifests/winsql.pp
-
+# Sample code to install MSSQLServer
 
 class win::winsql {
-  sqlserver_instance{'MSSQLServer':
+
+  sqlserver_instance{'ARSQLServer':
     features              =>  ['sql'],
     source                =>  'C:\Users\Administrator\Downloads',
     sql_sysadmin_accounts =>  ['mysql']
   }
   sqlserver::database { 'miniandydb':
-    instance  =>  'MSSQLServer',
+    instance  =>  'ARSQLServer',
   }
 }

7fa9f5de38e8699e2d05666559e1975650c3d701
Andy <andyr@puppet.com>
2017-02-01 11:05:31 -0800

    modified:   site/arnfs/manifests/init.pp

--- site/arnfs/manifests/init.pp
+++ site/arnfs/manifests/init.pp
@@ -10,8 +10,8 @@ class arnfs {
     server_enabled  => true
 
   }
-    nfs::server::export{ '/var/nfsshare':
-      ensure  => 'mounted',
-      clients => '192.168.0.0/16(rw,insecure,async,no_root_squash) localhost(rw)',
+  nfs::server::export{ '/var/nfsshare':
+    ensure  => 'mounted',
+    clients => '192.168.0.0/16(rw,insecure,async,no_root_squash) localhost(rw)',
   }
 }

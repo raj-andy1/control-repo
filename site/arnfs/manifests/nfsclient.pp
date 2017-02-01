486df228e3c806eadde2866d327bb52bf6c60169
Andy <andyr@puppet.com>
2017-02-01 07:52:49 -0800

    modified:   site/arnfs/manifests/nfsclient.pp

--- site/arnfs/manifests/nfsclient.pp
+++ site/arnfs/manifests/nfsclient.pp
@@ -1,2 +1,14 @@
 # /etc/puppetlabs/code/environments/production/site/arnfs/manifests/nfsclient.pp
 # Install nfs-client
+
+
+class arnfs::nfsclient {
+  $nfsservernm = 192.168.0.55
+
+  class {'::nfs':
+    client_enabled  => true,
+  }
+  Nfs::Client::Mount {'/var/nfsshare':
+    server  => $nfsservernm
+  }
+}

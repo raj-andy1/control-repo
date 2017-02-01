8a8d042027a2010c9f2da9385dd3303cf3b77afa
Andy <andyr@puppet.com>
2017-01-31 17:43:22 -0800

    modified:   site/winsql/manifests/armount.pp

--- site/winsql/manifests/armount.pp
+++ site/winsql/manifests/armount.pp
@@ -2,8 +2,8 @@
 # Mount an iso to a drive
 
 class winsql::armount {
-  $iso_source = 'C:\Users\Administrator\Downloads\SQLFULL_ENU.iso',
-  $iso_drive = 'X',
+  $iso_source = 'C:\Users\Administrator\Downloads\SQLFULL_ENU.iso'
+  $iso_drive = 'X'
 
   mount_iso { $iso_source:
     drive_letter  => $iso_drive,

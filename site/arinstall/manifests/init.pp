2a96f3ff99b3bb90f26df8d2b2ec091c76c39421
Andy <andyr@puppet.com>
2017-02-01 22:30:39 -0800

    new file:   site/arinstall/manifests/init.pp

---
+++ site/arinstall/manifests/init.pp
@@ -0,0 +1,11 @@
+# /etc/puppetlabs/code/environments/production/site/arinstall/manifests/init.pp
+# Sample code to install package from nfsshare
+
+class arinstall {
+
+  package {'tcpdump-4.5.1-3.el7.x86_64.rpm':
+    ensure   =>  installed,
+    provider => rpm,
+    source   =>  '/var/nfsshare',
+  }
+}

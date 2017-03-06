affdb0746dcbd1ff75fbee1dac1ac79f3194ae92
Andy <andyr@puppet.com>
2017-03-05 16:03:50 -0800

    modified:   Puppetfile new file:   site/arhiera/manifests/init.pp

---
+++ site/arhiera/manifests/init.pp
@@ -0,0 +1,15 @@
+# /etc/puppetlabs/code/environments/production/site/arhiera/manifests/init.pp
+# Sample Puppet code to implement eyaml for encrypting sensitive data
+
+class arhiera {
+  class { 'hiera':
+    hierarchy       => [
+      'secure',
+      'nodes/%{::trusted.certname}',
+      '%{environment}',
+      'common',
+    ],
+    eyaml           => true,
+    eyaml_extension => 'eyaml',
+  }
+}

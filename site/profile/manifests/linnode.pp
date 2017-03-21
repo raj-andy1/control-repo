2e851e6576e9aba573b3663e523f76a6d0b38afb
Andy <andyr@puppet.com>
2017-03-21 09:25:11 -0700

    modified:   site/profile/manifests/linnode.pp, add '' on class name

--- site/profile/manifests/linnode.pp
+++ site/profile/manifests/linnode.pp
@@ -13,7 +13,7 @@ class profile::linnode {
     ensure   =>  installed,
     provider => rpm,
     source   =>  '/var/nfsshare/tcpdump-4.5.1-3.el7.x86_64.rpm',
-    require  => Class[profile::nfsclient],
+    require  => Class['profile::nfsclient'],
   }
 
 }

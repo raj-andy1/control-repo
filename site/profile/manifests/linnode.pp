49d4c2fb4edfb9554fd7cf6e907b6868f7d06dc9
Andy <andyr@puppet.com>
2017-03-21 09:23:53 -0700

    modified:   site/profile/manifests/linnode.pp, using 'profile' to install

--- site/profile/manifests/linnode.pp
+++ site/profile/manifests/linnode.pp
@@ -6,6 +6,14 @@ class profile::linnode {
   #include arapache
   include arinifile
   #include aropenssh
-  include manage_users::testusers
+  #include manage_users::testusers
+
+
+  package {'tcpdump-4.5.1-3.el7.x86_64.rpm':
+    ensure   =>  installed,
+    provider => rpm,
+    source   =>  '/var/nfsshare/tcpdump-4.5.1-3.el7.x86_64.rpm',
+    require  => Class[profile::nfsclient],
+  }
 
 }

cdda3d14bbf1694c78efb01529f1674120f8cc03
Andy <andyr@puppet.com>
2017-04-28 06:36:11 -0700

    modified:   site/profile/manifests/linnode.pp

--- site/profile/manifests/linnode.pp
+++ site/profile/manifests/linnode.pp
@@ -14,21 +14,4 @@ class profile::linnode (
   class {'::ntp':
   servers => $ntpservernm,
   }
-
-  host {'nfsservernm':
-    ensure => present,
-    name   => $linnodenfsnm,
-    ip     => $linnodednsip,
-  }
-
-  package {'tcpdump-4.5.1-3.el7.x86_64.rpm':
-    ensure   =>  present,
-    provider => rpm,
-    source   =>  '/var/nfsshare/tcpdump-4.5.1-3.el7.x86_64.rpm',
-    require  => [
-      Class['profile::nfsclient'],
-      Host['nfsservernm'],
-      ],
-  }
-
 }

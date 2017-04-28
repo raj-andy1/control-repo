67a710506f77a340e66362cb37edb6c516848044
Andy <andyr@puppet.com>
2017-04-28 06:26:39 -0700

    modified:   site/profile/manifests/demohttpd.pp

--- site/profile/manifests/demohttpd.pp
+++ site/profile/manifests/demohttpd.pp
@@ -1,7 +1,9 @@
 # /etc/puppetlabs/code/environments/production/site/profile/manifests/demohttpd.pp
 # Sample code calling the arapache class
 
-class profile::demohttpd {
+class profile::demohttpd
+
+{
 
   include arapache
 

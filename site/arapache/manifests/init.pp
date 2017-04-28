ad5ba02e55016ecf34679aad6817bdd794a47aa2
Andy <andyr@puppet.com>
2017-04-28 06:43:55 -0700

    modified:   data/nodes/ar-demoagent03.yaml new file:   site/arapache/files/index1.html modified:   site/arapache/manifests/init.pp modified:   site/profile/manifests/base.pp

--- site/arapache/manifests/init.pp
+++ site/arapache/manifests/init.pp
@@ -1,13 +1,10 @@
 # /etc/puppetlabs/code/environments/production/site/arapache/manifests/init.pp
 # Sample Puppet code to install Apache on a centos machine and provide a sample webpage
 
-class arapache (
-  $http_version,
-  )
+class arapache {
 
-{
   package { 'httpd':
-    ensure  =>  $http_version,
+    ensure  =>  present,
   }
 
   service { 'httpd':
@@ -19,7 +16,7 @@ class arapache (
   file { '/var/www/html/index.html':
     ensure  =>  file,
     mode    =>  '0755',
-    source  =>  'puppet:///modules/arapache/index.html',
+    source  =>  'puppet:///modules/arapache/index1.html',
     require =>  Package['httpd'],
     notify  =>  Service['httpd'],
   }
--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,5 +1,36 @@
 class profile::base {
 
-  #the base profile should include component modules that will be on all nodes
 
+
+  #the base profile should include component modules that will be on all nodes
+  host {'nfsservernm':
+    ensure => present,
+    name   => $linnodenfsnm,
+    ip     => $linnodednsip,
+  }
+
+  package {'tcpdump-4.5.1-3.el7.x86_64.rpm':
+    ensure   =>  present,
+    provider => rpm,
+    source   =>  '/var/nfsshare/tcpdump-4.5.1-3.el7.x86_64.rpm',
+    require  => [
+      Class['profile::nfsclient'],
+      Host['nfsservernm'],
+      ],
+  }
+  host {'nfsservernm':
+    ensure => present,
+    name   => $linnodenfsnm,
+    ip     => $linnodednsip,
+  }
+
+  package {'tcpdump-4.5.1-3.el7.x86_64.rpm':
+    ensure   =>  present,
+    provider => rpm,
+    source   =>  '/var/nfsshare/tcpdump-4.5.1-3.el7.x86_64.rpm',
+    require  => [
+      Class['profile::nfsclient'],
+      Host['nfsservernm'],
+      ],
+  }
 }

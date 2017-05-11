a17ef98b8088c3012abe6b66ad21d0e8af781b42
Andy <andyr@puppet.com>
2017-05-11 15:40:28 -0700

    new file:   site/ar-tomcatapp/manifests/init.pp new file:   site/profile/manifests/ar-tomcatapp.pp

---
+++ site/ar-tomcatapp/manifests/init.pp
@@ -0,0 +1,2 @@
+#/etc/puppetlabs/code/environments/production/site/ar-tomcatapp/manifests/init.pp
+#sample code to demonstrate tomcat installation and Configurations
---
+++ site/profile/manifests/ar-tomcatapp.pp
@@ -0,0 +1,14 @@
+# /etc/puppetlabs/code/environments/production/profile/manifests/ar-tomcatapp.pp
+#sample code to demonstrate tomcat installation and Configurations
+
+tomcat::install { '/opt/tomcat':
+  source_url => 'https://www-us.apache.org/dist/tomcat/tomcat-7/v7.0.x/bin/apache-tomcat-7.0.x.tar.gz',
+}
+tomcat::instance { 'default':
+  catalina_home => '/opt/tomcat',
+}
+
+tomcat::config::server {'/opt/tomcat':
+  catalina_base =>  '/opt/tomcat',
+  port          =>  '8081'
+}

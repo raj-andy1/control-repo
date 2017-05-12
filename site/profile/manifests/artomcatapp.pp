cd5ac3a79daabf4a221f49a84e8fba1b258096eb
Andy <andyr@puppet.com>
2017-05-12 06:58:27 -0700

    modified:   site/profile/manifests/artomcatapp.pp

--- site/profile/manifests/artomcatapp.pp
+++ site/profile/manifests/artomcatapp.pp
@@ -1,6 +1,9 @@
 # /etc/puppetlabs/code/environments/production/profile/manifests/artomcatapp.pp
 #sample profile to demonstrate tomcat installation and Configurations
 class profile::artomcatapp {
+
+  include java
+
   tomcat::install { '/opt/tomcat':
     source_url => 'https://www-us.apache.org/dist/tomcat/tomcat-7/v7.0.77/bin/apache-tomcat-7.0.77.tar.gz',
   }

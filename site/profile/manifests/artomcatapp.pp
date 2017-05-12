0ff2fa366ca7a7221462a4410c74d9ffd600a9c5
Andy <andyr@puppet.com>
2017-05-12 06:28:46 -0700

    deleted:    site/profile/manifests/ar-tomcatapp.pp new file:   site/profile/manifests/artomcatapp.pp

---
+++ site/profile/manifests/artomcatapp.pp
@@ -0,0 +1,16 @@
+# /etc/puppetlabs/code/environments/production/profile/manifests/artomcatapp.pp
+#sample profile to demonstrate tomcat installation and Configurations
+class profile::artomcatapp {
+  tomcat::install { '/opt/tomcat':
+    source_url => 'https://www-us.apache.org/dist/tomcat/tomcat-7/v7.0.x/bin/apache-tomcat-7.0.x.tar.gz',
+  }
+
+  tomcat::instance { 'default':
+    catalina_home => '/opt/tomcat',
+  }
+
+  tomcat::config::server {'/opt/tomcat':
+    catalina_base =>  '/opt/tomcat',
+    port          =>  '8081'
+  }
+}

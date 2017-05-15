b31c708df04bcb8bea0fa284f4778466cfc7958f
Andy <andyr@puppet.com>
2017-05-14 21:06:38 -0700

    modified:   site/profile/manifests/artomcatapp.pp

--- site/profile/manifests/artomcatapp.pp
+++ site/profile/manifests/artomcatapp.pp
@@ -11,6 +11,7 @@ class profile::artomcatapp {
   tomcat::instance { 'default':
     catalina_home => '/opt/tomcat',
   }
+
   tomcat::war {'sample.war':
     catalina_base => '/opt/tomcat',
     war_source    => 'https://tomcat.apache.org/tomcat-6.0-doc/appdev/sample/sample.war',

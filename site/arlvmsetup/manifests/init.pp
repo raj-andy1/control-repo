784a0282e463d152b05bdad8e29c9fa89b0805e6
Andy <andyr@puppet.com>
2017-05-15 17:29:26 -0700

    modified:   site/arlvmsetup/manifests/init.pp

--- site/arlvmsetup/manifests/init.pp
+++ site/arlvmsetup/manifests/init.pp
@@ -2,26 +2,14 @@
 # sample profile to demonstrate puppet lvm
 class arlvmsetup {
 
-include lvm
-
-physical_volume { '/dev/sdb':
-  ensure => present,
-}
-
-volume_group { 'myvg':
-  ensure           => present,
-  physical_volumes => '/dev/hdc',
-}
-
-logical_volume { 'mylv':
-  ensure       => present,
-  volume_group => 'myvg',
-  size         => '20G',
-}
-
-filesystem { '/myvg/mylv':
-  ensure  => present,
-  fs_type => 'ext3',
-  options => '-b 4096 -E stride=32,stripe-width=64',
+  class { 'lvm':
+    volume_groups    => {
+      'myvg' => {
+        physical_volumes => '/dev/sda2',
+        logical_volumes  => {
+          'yah'    => {'size' => '8G'},
+          },
+        },
+      },
+    },
   }
-}

a6dbcb5d433230d4c6c3177f5e8d8576d7a5d9c5
Andy <andyr@puppet.com>
2017-05-17 05:16:54 -0700

    modified:   site/profile/manifests/artomcatapp.pp new file:   site/windemo/manifests/init.pp new file:   site/windemo/manifests/uc1.pp new file:   site/windemo/manifests/uc2.pp new file:   site/windemo/manifests/uc3.pp new file:   site/windemo/manifests/uc4.pp

--- site/profile/manifests/artomcatapp.pp
+++ site/profile/manifests/artomcatapp.pp
@@ -1,6 +1,7 @@
 # /etc/puppetlabs/code/environments/production/profile/manifests/artomcatapp.pp
 # sample profile to demonstrate tomcat installation on a new filesystem and Configurations
 class profile::artomcatapp {
+
   include artomcatsetup
   include arlvmsetup
 

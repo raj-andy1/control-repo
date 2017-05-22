f081786ca085eeaad2bd2b01b6a26a01c83a2919
Andy <andyr@puppet.com>
2017-05-22 16:23:10 -0700

    modified:   site/artomcatsetup/manifests/init.pp

--- site/artomcatsetup/manifests/init.pp
+++ site/artomcatsetup/manifests/init.pp
@@ -1,20 +1,23 @@
 #/etc/puppetlabs/code/environments/production/site/artomcatsetup/manifests/init.pp
 #sample code to demonstrate tomcat installation and Configurations
 
-class artomcatsetup {
+class artomcatsetup (
+  $tomcatinstalldir = '/opt/tomcat',
+  )
 
+{
   include java
 
-  tomcat::install { '/yah/tomcat':
-    source_url => 'http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.77/bin/apache-tomcat-7.0.77.tar.gz',
+  tomcat::install { $tomcatinstalldir:
+    source_url => 'http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.78/bin/apache-tomcat-7.0.78.tar.gz',
   }
 
   tomcat::instance { 'default':
-    catalina_home => '/yah/tomcat',
+    catalina_home => $tomcatinstalldir,
   }
 
   tomcat::war {'sample.war':
-    catalina_base => '/yah/tomcat',
+    catalina_base => $tomcatinstalldir,
     war_source    => 'https://tomcat.apache.org/tomcat-6.0-doc/appdev/sample/sample.war',
   }
 }

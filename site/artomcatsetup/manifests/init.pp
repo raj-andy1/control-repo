304839890c8204520ed40b023a4150a590a257e5
Andy <andyr@puppet.com>
2017-05-22 13:27:00 -0700

    modified:   site/artomcatsetup/manifests/init.pp

--- site/artomcatsetup/manifests/init.pp
+++ site/artomcatsetup/manifests/init.pp
@@ -6,7 +6,7 @@ class artomcatsetup {
   include java
 
   tomcat::install { '/yah/tomcat':
-    source_url => 'https://www-us.apache.org/dist/tomcat/tomcat-7/v7.0.78/bin/apache-tomcat-7.0.78.tar.gz',
+    source_url => 'http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.77/bin/apache-tomcat-7.0.77.tar.gz',
   }
 
   tomcat::instance { 'default':

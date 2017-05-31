ec795922c24248c51b197d05c85cc7e56db5758a
Andy <andyr@puppet.com>
2017-05-31 06:12:13 -0700

    modified:   data/common.yaml, set runinterval variable for common new file:   data/nodes/ip-10-90-10-93.us-west-2.compute.internal.yaml, set runinterval variable for the node modified:   site/arinifile/manifests/init.pp, set runinterval variable

--- site/arinifile/manifests/init.pp
+++ site/arinifile/manifests/init.pp
@@ -1,12 +1,16 @@
 # /etc/puppetlabs/code/environments/production/site/arinifile/manifests/init.pp
 # Sample code to change Puppet agent run interval to 10d from 30m
 
-class arinifile {
-  ini_setting { 'change runinterval to 10d':
+class arinifile (
+  $runint,
+  )
+
+{
+  ini_setting { 'change runinterval':
     ensure  => present,
     path    => '/etc/puppetlabs/puppet/puppet.conf',
     section => 'agent',
     setting => 'runinterval',
-    value   => '10d',
+    value   => $runint,
   }
 }

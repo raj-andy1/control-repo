0887e87fa246dd3eb4da95db8fef37898a2fd3db
Andy <andyr@puppet.com>
2017-07-12 15:59:38 -0700

    removed package lvm dependency

--- site/arlvmsetup/manifests/init.pp
+++ site/arlvmsetup/manifests/init.pp
@@ -1,15 +1,6 @@
 # /etc/puppetlabs/code/environments/production/site/arlvmsetup/manifests/init.pp
 # sample profile to demonstrate puppet lvm
 class arlvmsetup {
-
-  package {'lvm2':
-    ensure => present,
-  }
-
-  file {'/yah':
-    ensure => directory
-  }
-
   class { 'lvm':
     require       => [
       Package['lvm2'],
@@ -22,7 +13,6 @@ class arlvmsetup {
           'yah' => {
             'size'              => '6G',
             'mountpath'         => '/yah',
-            'mountpath_require' => true,
           },
         },
       },

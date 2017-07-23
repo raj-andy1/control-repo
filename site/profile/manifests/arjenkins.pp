01cc054657a000948ebb46567d81a786951ef1f4
Andy <andyr@puppet.com>
2017-07-22 23:04:10 -0700

    modified:   site/profile/manifests/arjenkins.pp

--- site/profile/manifests/arjenkins.pp
+++ site/profile/manifests/arjenkins.pp
@@ -1,6 +1,6 @@
 # /etc/puppetlabs/code/environments/production/site/profile/manifests/arjenkins.pp
-# Sample code to install Jenkins
+# Sample code to install Jenkins master
 
 class profile::arjenkins {
-  
+  include jenkins
 }

3a37250fefc9c1447270a68fb9e98e4817f670e8
Andy <andyr@puppet.com>
2017-07-23 10:12:06 -0700

    modified:   site/profile/manifests/arjenkins.pp

--- site/profile/manifests/arjenkins.pp
+++ site/profile/manifests/arjenkins.pp
@@ -2,20 +2,32 @@
 # Sample code to install Jenkins master
 
 class profile::arjenkins {
-  class {'jenkins::plugins':
-    plugin_hash => {
-      'git' => { version => '3.4.1' },
-      'workflow-scm-step' => { version => '1.14.2' },
-      'Credentials' => { version => '2.1.14'},
-      'structs' => { version => '1.7'},
-      'puppet-enterprise-pipeline' => { version => '1.3.1'},
-      'workflow-api' => { version => '2.3' },
-      'workflow-basic-steps' => { version => '2.0'},
-      'workflow-cps' => { version => '2.17'},
-      'workflow-durable-task-step' => { version =>'2.4'},
-      'credentials' => { version => '1.21' },
-      'plain-credentials' => { version => '1.2' },
-      'script-security' => { version => '1.22' },
-    }
-  }
+
+  include jenkins
+
+  jenkins::plugin_hash:
+    'git' :
+      version : '3.4.1'
+    'workflow-scm-step' :
+      version : '1.14.2'
+    'Credentials' :
+      version : '2.1.14'
+    'structs' :
+      version : '1.7'
+    'puppet-enterprise-pipeline' :
+      version : '1.3.1'
+    'workflow-api' :
+      version : '2.3'
+    'workflow-basic-steps' :
+      version : '2.0'
+    'workflow-cps' :
+      version : '2.17'
+    'workflow-durable-task-step' :
+      version :'2.4'
+    'credentials' :
+      version : '1.21'
+    'plain-credentials' :
+      version : '1.2'
+    'script-security' :
+      version : '1.22' 
 }

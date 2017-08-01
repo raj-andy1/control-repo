444fa972e50ebe27495845eb812a508ba414dc5a
Andy <andyr@puppet.com>
2017-08-01 09:52:59 -0700

    new file:   site/profile/manifests/artesting.pp

---
+++ site/profile/manifests/artesting.pp
@@ -0,0 +1,15 @@
+# /etc/puppetlabs/code/environments/production/site/profile/manifests/artesting.pp
+# Sample code to prep a test Server
+
+class profile::artesting {
+  package {"rubygems": ensure => present,}
+  package {"git": ensure => present,}
+  package {"make": ensure => present,}
+  package {"gcc": ensure => present,}
+  package {"gcc-c++": ensure => present,}
+  package {"libxml2-devel": ensure => present,}
+  package {"libxslt-devel": ensure => present,}
+  package {"beaker": ensure => present, provider => puppet_gem, require => Package['rubygems']}
+  package {"puppet-lint": provider => puppet_gem, require => Package['rubygems']}
+  package {"onceover": provider => puppet_gem, require => Package['rubygems']}
+}

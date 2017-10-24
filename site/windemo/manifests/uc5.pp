78199a5b82e2f2453569c52d193c760d6c370a97
Andy <andyr@puppet.com>
2017-10-24 14:00:03 -0700

    modified:   site/windemo/manifests/uc5.pp

--- site/windemo/manifests/uc5.pp
+++ site/windemo/manifests/uc5.pp
@@ -14,7 +14,7 @@ class windemo::uc5 {
     } ->
 
     exec {'set-dhcp-securitygroups':
-      command => 'netsh dhcp add securitygroups',
+      command => 'C:\Windows\System32\netsh dhcp add securitygroups',
       creates => 'C:\created.txt',
     } ~>
 

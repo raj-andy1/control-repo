28a71a818bcecdc1e35e74dd7e841227518813b5
Andy <andyr@puppet.com>
2017-10-23 21:42:16 -0700

    modified:   site/windemo/manifests/uc5.pp

--- site/windemo/manifests/uc5.pp
+++ site/windemo/manifests/uc5.pp
@@ -8,15 +8,16 @@ class windemo::uc5 (
     dsc_windowsfeature { '$windowsfeaturenm':
       ensure   =>  present,
       dsc_name => $windowsfeaturenm,
-      dsc_xDhcpServerScope => {
-          ensure => present,
-          dsc_ipendrange => '192.168.1.254',
-          dsc_ipstartrange => '192.168.1.1',
-          dsc_subnetmask => '255.255.255.0',
-          dsc_leaseduration => '00:08:00',
-          dsc_state => 'active',
-          dsc_addressfamily => 'ipv4',
-          dsc_name => 'samplescope',
-        }
+    }
+
+    dsc_xDhcpServerScope { 'samplescope':
+      ensure => present,
+      dsc_ipendrange => '192.168.1.254',
+      dsc_ipstartrange => '192.168.1.1',
+      dsc_subnetmask => '255.255.255.0',
+      dsc_leaseduration => '00:08:00',
+      dsc_state => 'active',
+      dsc_addressfamily => 'ipv4',
+      dsc_name => 'samplescope',
     }
   }

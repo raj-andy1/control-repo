c2dd17c1f200e19e6089c6ef4f69f5f24169f915
Andy <andyr@puppet.com>
2017-10-24 16:56:42 -0700

    modified:   site/windemo/manifests/uc5.pp

--- site/windemo/manifests/uc5.pp
+++ site/windemo/manifests/uc5.pp
@@ -3,24 +3,27 @@
 
 class windemo::uc5 {
 
+#install DHCP Server feature
     dsc_windowsfeature { 'DHCP':
       ensure   =>  present,
       dsc_name => 'DHCP',
     } ->
-
+#install DHCP Server Remote Server Administration
     dsc_windowsfeature { 'RSAT-DHCP':
       ensure   =>  present,
       dsc_name => 'RSAT-DHCP',
     } ->
 
+# Add DHCP Security groups
     exec {'set-dhcp-securitygroups':
       command => 'C:\windows\system32\netsh.exe dhcp add securitygroups',
-      onlyif => 'C:\windows\system32\net.exe "DHCP Admistrators"',
+      onlyif => 'C:\windows\system32\net.exe "DHCP Admistrators"', #Run only if the "DHCP Administrators" group has not been added
     } ~>
-
+# Restart DHCP Service after adding the DHCP Security groups to complete the installation process
     service {'DHCPServer':
   } ->
 
+# Add sample DHCP Scope
     dsc_xDhcpServerScope { 'samplescope':
       ensure => present,
       dsc_ipendrange => '172.16.1.254',

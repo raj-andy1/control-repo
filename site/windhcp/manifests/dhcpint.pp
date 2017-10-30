238c356c429f22a186c7f58b77ca147bcdb1ff45
Andy <andyr@puppet.com>
2017-10-30 15:35:13 -0700

    modified:   site/windhcp/manifests/dhcpint.pp

--- site/windhcp/manifests/dhcpint.pp
+++ site/windhcp/manifests/dhcpint.pp
@@ -3,7 +3,6 @@
 
 class windhcp::dhcpint {
   $::interface_guids.each | $key, $value| {
-    SYSTEM\ControlSet001\Services\Tcpip\Parameters\Interfaces\{C0E7C938-9118-414C-B33B-54AD9FE8E4A9
     registry_value { "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Tcpip\\Parameters\\Interfaces\\{${value}}\\EnableDHCP":
       ensure => present,
       type   => dword,

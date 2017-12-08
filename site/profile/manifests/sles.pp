e0287bba5d1e655695ce40e299e76b92ed6ed0cc
Andy Rajagopalan <andyr@Andys-MBP.attlocal.net>
2017-12-08 15:30:46 -0800

    modified:   site/profile/manifests/sles.pp

--- site/profile/manifests/sles.pp
+++ site/profile/manifests/sles.pp
@@ -1,7 +1,7 @@
 # /etc/puppetlabs/code/environments/production/profile/manifests/sles.pp
 # Example code to setup Suse Enterprise Linux
 
-clas profile::sles {
+class profile::sles {
   package { 'cron':
     ensure => installed,
   }

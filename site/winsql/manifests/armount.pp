b50bfbc3f1436a4a6dfc05995c76a7ffd31547cd
Andy Rajagopalan <andyr@Andys-MBP.attlocal.net>
2017-12-14 09:38:38 -0800

    modified:   site/winsql/manifests/armount.pp

--- site/winsql/manifests/armount.pp
+++ site/winsql/manifests/armount.pp
@@ -2,7 +2,8 @@
 # Mount an iso to a drive
 
 class winsql::armount (
-  $iso_source = 'C:\Users\Administrator\Downloads\SQLServer2014SP2-FullSlipstream-x64-ENU.iso',
+  #$iso_source = 'C:\Users\Administrator\Downloads\SQLServer2014SP2-FullSlipstream-x64-ENU.iso',
+  $iso_source = 'https://s3-us-west-2.amazonaws.com/arsqldemo/SQLServer2014SP2-FullSlipstream-x64-ENU.iso',
   $iso_drive = 'X',
 )
 

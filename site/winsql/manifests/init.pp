b5d111176c2ed798afc4456a11239ad62b7d119a
Andy Rajagopalan <andyr@Andys-MBP.attlocal.net>
2017-12-14 06:54:04 -0800

    modified:   site/winsql/manifests/init.pp

--- site/winsql/manifests/init.pp
+++ site/winsql/manifests/init.pp
@@ -1,1 +1,11 @@
 # /etc/puppetlabs/code/environments/production/site/winsql/manifests/init.pp
+
+
+class winsql {
+  sqlserver_instance { 'MSSQLSERVER':
+    features  => ['SQL'],
+    source  =>  'X:\',
+    require => Class['winsql::armount'],
+    sql_sysadmin_accounts => ['myuser'],
+  }
+}

98a0add5d3b61b0e6ecb47169171e9d9ba6233f0
raj-andy1 <andyr@Andys-MBP.attlocal.net>
2018-02-09 13:31:41 -0800

    new file:   site/profile/manifests/arwinautoupdate.pp

---
+++ site/profile/manifests/arwinautoupdate.pp
@@ -0,0 +1,6 @@
+# /etc/puppetlabs/code/environments/production/site/profile/manifests/arwinautoupdate.pp
+# Sample class for configuring windows autoupdate settings
+
+class profile::arwinautoupdate {
+  class { 'windows_autoupdate': noAutoUpdate => '1' }
+}

9a2a027193e6999cebce48d2752991504ffcf58d
raj-andy1 <andyr@Andys-MBP.attlocal.net>
2018-02-09 22:57:39 -0800

    new file:   site/araws/manifests/dnsprov.pp deleted:    site/araws/manifests/provision.pp

---
+++ site/araws/manifests/dnsprov.pp
@@ -0,0 +1,11 @@
+# /etc/puppetlabs/code/environments/production/site/andyaws/manifests/dnsprov.pp
+# Sample code to create DNS A records
+
+class araws::dnsprov {
+  route53_a_record {'demoagent03.armusings.info':
+    ensure  => present,
+    values  => ['54.212.230.58'],
+    zone => 'armusings.info'
+
+  }
+}

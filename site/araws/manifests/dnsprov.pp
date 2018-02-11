0269e2b3f4bb89dd8aa0a7b5d803f8e7421b8beb
raj-andy1 <andyr@Andys-MBP.attlocal.net>
2018-02-10 22:54:07 -0800

    modified:   site/araws/manifests/dnsprov.pp

--- site/araws/manifests/dnsprov.pp
+++ site/araws/manifests/dnsprov.pp
@@ -2,10 +2,10 @@
 # Sample code to create DNS A records
 
 class araws::dnsprov {
-  route53_a_record {'demoagent03.armusings.info.':
+  route53_a_record {"$linprov::inst_nm.":
     ensure  => present,
     ttl => '300',
-    values  => ['54.212.230.58'],
+    values  => ["$::ec2_publicip"],
     zone => 'armusings.info.',
   }
 }

06e40517481e0b2b3fe8eb6dcda325d73c1a0f97
raj-andy1 <andyr@Andys-MacBook-Pro.local>
2018-02-13 10:27:52 -0800

    modified:   site/araws/manifests/dnsprov.pp

--- site/araws/manifests/dnsprov.pp
+++ site/araws/manifests/dnsprov.pp
@@ -2,8 +2,9 @@
 # Sample code to create DNS A records
 
 class araws::dnsprov {
-  
+
  notify {"${trusted[extensions][pp_hostname]}.":}
+ notify {"${ec2_metadata[public-ipv4]}":}
 
  route53_a_record {"{$trusted.extensions.pp_hostname}.":
     ensure  => present,

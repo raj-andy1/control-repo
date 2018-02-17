560a41ab63b60f8fc7bc3880b97f3c048b4aeabd
Anandthirth Rajagopalan <andyr@mac-c02sg1zvfvh8.local>
2018-02-16 16:12:33 -0800

    new profile

--- site/profile/manifests/arawsinstance.pp
+++ site/profile/manifests/arawsinstance.pp
@@ -1,2 +1,9 @@
 # /etc/puppetlabs/code/environments/production/site/profile/manifests/awsinstance.pp
 # Sample code to setup an aws instance
+
+class profile::arawsinstance () {
+
+  package { 'aws-sdk':
+  ensure => present, 
+  }
+}

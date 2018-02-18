8742ce4f075a88f6922f5f8c7a865301d698cc1d
Anandthirth Rajagopalan <andyr@mac-c02sg1zvfvh8.local>
2018-02-18 06:13:47 -0800

    removed spaces

--- site/profile/manifests/arawsinstance.pp
+++ site/profile/manifests/arawsinstance.pp
@@ -1,9 +1,10 @@
 # /etc/puppetlabs/code/environments/production/site/profile/manifests/awsinstance.pp
 # Sample code to setup an aws instance
 
-class profile::arawsinstance () {
+class profile::arawsinstance {
+
   include araws::instancesetup
   include araws::dnsprov
-
-  Class ['araws::instancesetup'] -> Class ['araws::dnsprov']
+  
+  Class['araws::instancesetup'] -> Class['araws::dnsprov']
 }

a598cf556c3a473828785313ccf1ef6eb5ff15e2
Anandthirth Rajagopalan <andyr@mac-c02sg1zvfvh8.attlocal.net>
2018-02-18 06:20:00 -0800

    changed hostname vars

--- site/araws/manifests/linprov.pp
+++ site/araws/manifests/linprov.pp
@@ -2,7 +2,8 @@
 # Sample code to provision an AWS instance running RHEL
 
 class araws::linprov (
-  $pe_master = 'ec2-54-186-186-20.us-west-2.compute.amazonaws.com',
+  $pe_master = 'puppetmaster01.armusings.info'
+  #'ec2-54-186-186-20.us-west-2.compute.amazonaws.com',
   $inst_nm = 'AndyR - Jenkins Server',
   $aws_region = 'us-west-2',
   $avza = 'us-west-2a',

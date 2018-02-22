8cea0b68399839851a38c284fbe68c322c16c0c7
Anandthirth Rajagopalan <andyr@mac-c02sg1zvfvh8.attlocal.net>
2018-02-22 08:14:50 -0800

    modified uc6

---
+++ site/windemo/manifests/uc6.pp
@@ -0,0 +1,34 @@
+#
+#
+
+class windemo::uc6 {
+
+# Remote Desktop Connection
+registry::value { 'rdp':
+  key   => 'HKLM\System\CurrentControlSet\Control\Terminal Server',
+  value => 'fDenyTSConnections',
+  type  => 'dword',
+  data  => '0', # '0' to enable or '1' to disable
+}
+
+# Remote Desktop Network Level Authentication
+registry::value { 'rdp_nla':
+  key   => 'HKLM\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp',
+  value => 'UserAuthentication',
+  type  => 'dword',
+  data  => '0', # '0' to enable or '1' to disable
+}
+
+# Enable RDP Firewall Rule (Windows firewall is NOT enabled but the required rule is created)
+
+windows_firewall::exception { 'rdp_firewall':
+  ensure       => present,
+  direction    => 'in',
+  action       => 'allow',
+  enabled      => 'yes',
+  protocol     => 'TCP',
+  local_port   => '3389',
+  display_name => 'Remote Desktop - Puppet',
+  description  => 'This exception allows RDP access (TCP 3389)',
+  }
+}

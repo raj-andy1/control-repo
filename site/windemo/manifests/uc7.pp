7862a87b52aa9dd3e17aea5f44ea3ed9bb617a23
Anandthirth Rajagopalan <andyr@mac-c02sg1zvfvh8.local>
2018-03-02 09:47:44 -0800

    add .exe

--- site/windemo/manifests/uc7.pp
+++ site/windemo/manifests/uc7.pp
@@ -2,14 +2,14 @@
 # Sample Puppet code to demonstrate installation of packages from a share
 
 class windemo::uc7 {
-  file {'C:\Windows\Temp\GoogleChromeStandaloneEnterprise64':
+  file {'C:\Windows\Temp\GoogleChromeStandaloneEnterprise64.exe':
   ensure => file,
-  source => 'puppet:///ar/GoogleChromeStandaloneEnterprise64',
+  source => 'puppet:///ar/GoogleChromeStandaloneEnterprise64.exe',
 
   }
   package {'Google Chrome':
   ensure => installed,
-  source => 'C:\Windows\Temp\GoogleChromeStandaloneEnterprise64',
+  source => 'C:\Windows\Temp\GoogleChromeStandaloneEnterprise64.exe',
   install_options => ['/SILENT'],
   }
 }

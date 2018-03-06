5501a0d1f80c7eee7666d51be9b28894b8f418ed
Anandthirth Rajagopalan <andyr@mac-c02sg1zvfvh8.local>
2018-03-06 14:15:52 -0800

    new changes to arcifsmount

---
+++ site/profile/manifests/arcifsmount.pp
@@ -0,0 +1,22 @@
+# /etc/puppetlabs/code/environments/production/site/profile/manifests/arcifsmount.pp
+# Sample code to mount a CIFS volume on a linux VM
+
+class profile::arcifsmount (
+  $mnt_usernm = 'test-user01',
+  $mnt_passwd = 'Puppetlabs01',
+)
+
+{ 
+  package { 'cifs-utils':
+    ensure => 'present.'
+  } ->
+
+mount { '/ar':
+device => '//ar-win-1/ar',
+atboot => true,
+ensure => mounted,
+fstype => 'cifs',
+options => 'auto,user=test-user01,pass="Puppetlabs01"'
+  }
+}
+
--- site/sutterh/manifests/init.pp
+++
@@ -1,6 +0,0 @@
-# /etc/puppetlabs/code/environments/production/site/sutterh/manifests/init.pp
-# Sample code to demonstrate Puppet for Sutter Health
-
-class sutterh {
-
-}

0f0d92bb2381464d707a0471c1fa1bfaa27cc39f
Anandthirth Rajagopalan <andyr@mac-c02sg1zvfvh8.local>
2018-03-20 20:56:37 -0700

    new file for install exe file w/ space

---
+++ site/windemo/manifests/uc8.pp
@@ -0,0 +1,15 @@
+# /etc/puppetlabs/code/environments/production/site/windemo/manifests/uc8.pp
+# Sample Puppet code to demonstrate installation of packages from a share
+
+class windemo::uc8 {
+  file {'C:\Windows\Temp\npp.7.5.5.Installer.x64.exe':
+  ensure => file,
+  source => 'puppet:///ar/Windows exe/npp.7.5.5.Installer.x64.exe',
+
+  }
+  package {'Notepad++':
+  ensure => installed,
+  source => 'C:\Windows\Temp\npp.7.5.5.Installer.x64.exe',
+  install_options => ['/S'],
+  }
+}

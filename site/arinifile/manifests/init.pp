b3a87e57183a7b64b66993a19301b552f05b8193
Anandthirth Rajagopalan <andyr@mac-c02sg1zvfvh8.local>
2018-03-23 07:05:41 -0700

    add default value for runint and change os fact

--- site/arinifile/manifests/init.pp
+++ site/arinifile/manifests/init.pp
@@ -2,11 +2,11 @@
 # Sample code to change Puppet agent run interval to 10d from 30m
 
 class arinifile (
-  $pathnm = $facts['operatingsystem'] ? {
+  $pathnm = $::operatingsystem ? {
     'windows' => 'C:/ProgramData/PuppetLabs/puppet/etc/puppet.conf',
     /(CentOS|RedHat)/ => '/etc/puppetlabs/puppet/puppet.conf',
   },
-  $runint,
+  $runint = '3600s',
   )
 
 {

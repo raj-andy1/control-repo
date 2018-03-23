455a8fd4652b48c08a0111b1b93419595eb857e5
Anandthirth Rajagopalan <andyr@mac-c02sg1zvfvh8.attlocal.net>
2018-03-23 08:18:34 -0700

    added comment on dependency

--- site/arinifile/manifests/init.pp
+++ site/arinifile/manifests/init.pp
@@ -1,4 +1,5 @@
 # /etc/puppetlabs/code/environments/production/site/arinifile/manifests/init.pp
+# dependent on the https://forge.puppet.com/puppetlabs/inifile
 # Sample code to change Puppet agent run interval to 10d from 30m
 
 class arinifile (

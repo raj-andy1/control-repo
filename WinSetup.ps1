4dd127fa90950930eab4016f86c8d0111fb5f9a1
Anandthirth Rajagopalan <andyr@mac-c02sg1zvfvh8.local>
2018-05-29 18:58:11 -0700

    changed puppet master info

--- WinSetup.ps1
+++ WinSetup.ps1
@@ -1,5 +1,5 @@
 #ps1
 # Setup script for preparing windows virtual machines to work with Puppet master
 
-ac -Encoding UTF8  C:\Windows\system32\drivers\etc\hosts "192.168.0.10	master4.localdomain	master4"
-[Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}; $webClient = New-Object System.Net.WebClient; $webClient.DownloadFile('https://master4:8140/packages/current/install.ps1', 'install.ps1'); .\install.ps1 extension_requests:pp_role=demo-webserver extension_requests:pp_created_by=AndyR extension_requests:pp_department=TSE
+ac -Encoding UTF8  C:\Windows\system32\drivers\etc\hosts "192.168.0.10	ar-pe-master01.localdomain	ar-pe-master01"
+[Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}; $webClient = New-Object System.Net.WebClient; $webClient.DownloadFile('https://ar-pe-master01:8140/packages/current/install.ps1', 'install.ps1'); .\install.ps1 extension_requests:pp_role=dhcp-server extension_requests:pp_created_by=AndyR

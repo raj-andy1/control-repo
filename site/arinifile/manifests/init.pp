8b679980f06c483d16521d04918e7f2c8c02dec4
Anandthirth Rajagopalan <andyr@mac-c02sg1zvfvh8.local>
2018-05-30 19:42:15 -0700

    add addtional module description

--- site/arinifile/manifests/init.pp
+++ site/arinifile/manifests/init.pp
@@ -1,6 +1,7 @@
 # /etc/puppetlabs/code/environments/production/site/arinifile/manifests/init.pp
 # dependent on the https://forge.puppet.com/puppetlabs/inifile
 # Sample code to change Puppet agent run interval to 10d from 30m
+# Based on the operating system Windows or *nix(default) the code will pick the appropriate puppet.conf file
 
 class arinifile (
   $pathnm = $::operatingsystem ? {

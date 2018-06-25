b6cfb0451b8d771c4c246152a092b61029f43848
Anandthirth Rajagopalan <andyr@mac-c02sg1zvfvh8.attlocal.net>
2018-06-25 12:40:21 -0700

    add puppet master dns name

--- site/araws/manifests/linprov.pp
+++ site/araws/manifests/linprov.pp
@@ -2,20 +2,20 @@
 # Sample code to provision an AWS instance running RHEL
 
 class araws::linprov (
-  $pe_master = 'ec2-54-186-186-20.us-west-2.compute.amazonaws.com',
-  $inst_nm = 'AndyR - Jenkins Server',
+  $pe_master = 'puppetmaster01.ar-aws.tsedemos.com',
+  $inst_nm = 'ar-web01',
   $aws_region = 'us-west-2',
   $avza = 'us-west-2a',
   $img_id = 'ami-b55a51cc', #RHEL AMI id
   $inst_type = 't2.large',
   $snet = 'tse-us-west-2-avza',
   $secg = ['tse-us-west-2-crossconnect','tse-us-west-2-agents'],
-  $iam_ip_arn = 'arn:aws:iam::610189707431:instance-profile/IAM_DNS_Role',
+  $iam_ip_arn = '', #arn:aws:iam::610189707431:instance-profile/IAM_DNS_Role
   $add_vol = false,
   $vol_nm = '/dev/sdb',
   $vol_sz = '8',
   $key_nm = 'andy.rajagopalan',
-  $pe_role = 'jenkins-master',
+  $pe_role = 'webserver',
   )
   {
   ec2_instance { $inst_nm:

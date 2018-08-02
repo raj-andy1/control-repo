ce3fa219529c98cc2c877be6242871b90066a6f9
Andy Rajagopalan <ranandthirth@@gmail.com>
2018-08-02 10:00:20 -0700

    install the apache mod_wsgi  module

--- site/profile/manifests/mypythonapp.pp
+++ site/profile/manifests/mypythonapp.pp
@@ -10,14 +10,17 @@ class profile::mypythonapp (
   Package { ensure => 'installed'}
   package { $packages: }
   package { $pip_packages: provider => 'pip'}
+  package { 'wandisco-git': source => 'http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-2.noarch.rpm'} ->
+  package {'git':}
 
   class {'apache':
   default_vhost => 'false'
   }
 
+  apache::mod { 'mod_wsgi':}
+
   apache::vhost {'mypythonapp.ar-gcp.tsedemos.com':
   port => '80'
   docroot => '/var/www/mypythonapp'
-
   }
 }

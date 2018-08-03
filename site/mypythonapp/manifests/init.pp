b73fad53aa851df810481c05702deae1c08595e1
Andy Rajagopalan <ranandthirth@@gmail.com>
2018-08-03 10:34:00 -0700

    new files for trying out image_build module

---
+++ site/mypythonapp/manifests/init.pp
@@ -0,0 +1,56 @@
+# /etc/puppetlabs/code/environments/production/site/mypythonapp/manifests/init.pp
+# Sample Puppet code to setup apache and other stuff for mypythonapp
+
+class mypythonapp (
+  $packages = ['python-pip'],
+  $pip_packages = ['flask','flask-mysql']
+)
+
+{
+  Package { ensure => 'installed'}
+  package { $packages: } ->
+  package { $pip_packages: provider => 'pip'}
+  package { 'wandisco-git-release': source => 'http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-2.noarch.rpm'} ->
+  package {'git':}
+
+  class {selinux:
+  mode => 'permissive',
+  type => 'targeted',
+  }
+
+  file {'/var/www/mypythonapp':
+  ensure => directory,
+  mode => '0755',
+  owner => 'apache',
+  group => 'apache',
+  } ->
+  vcsrepo {'/var/www/mypythonapp':
+  ensure => present,
+  provider => git,
+  source => 'git://github.com/raj-andy1/PythonApp.git',
+  require => Package['git'],
+  }
+  
+  class {'apache':
+  default_vhost => false,
+  }
+  
+  class {'apache::mod::wsgi':}
+  
+  apache::vhost {'mypythonapp.ar-gcp.tsedemos.com':
+  port => '80',
+  priority => '10',
+  docroot => '/var/www/mypythonapp',
+  wsgi_application_group      => '%{GLOBAL}',
+  wsgi_daemon_process         => 'pythonapp',
+  wsgi_daemon_process_options => {
+    user => 'apache',
+    group => 'apache',
+    threads      => '15',
+    python-path => '/var/www/mypythonapp'
+  },
+  wsgi_import_script          => '/var/www/mypythonapp/pythonapp.wsgi',
+  wsgi_process_group          => 'pythonapp',
+  wsgi_script_aliases         => { '/' => '/var/www/mypythonapp/pythonapp.wsgi' }
+  }
+}
